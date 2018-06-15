7d6198f3260bc5e78f833f4e64e7a32dd391255e
Giovani Nascimento Pereira <giovani.x.pereira@gmail.com>
2018-06-15 09:25:13 -0300

    Created watch project

---
+++ Metronome WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Metronome WatchKit Extension
+//
+//  Created by Giovani Nascimento Pereira on 15/06/18.
+//  Copyright © 2018 Giovani Nascimento Pereira. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
