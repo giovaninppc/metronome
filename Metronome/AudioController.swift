aada8dce7ceada1fd481c7fa00244acb1be82eb5
Giovani Nascimento Pereira <giovani.x.pereira@gmail.com>
2018-06-18 16:32:43 -0300

    updates

---
+++ Metronome/AudioController.swift
@@ -0,0 +1,41 @@
+//
+//  AudioController.swift
+//  Metronome
+//
+//  Created by Giovani Nascimento Pereira on 18/06/18.
+//  Copyright © 2018 Giovani Nascimento Pereira. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class AudioController {
+    
+    static var player: AVAudioPlayer?
+    
+    static func playLowBeep() {
+        if let url = Bundle.main.url(forResource: "metronome_click", withExtension: "mp3") {
+            startSong(url)
+        } else {
+            return
+        }
+    }
+    
+    private static func startSong(_ url: URL) {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            guard let backgroundPlayer = player else { return }
+            
+            backgroundPlayer.volume = 0.8
+            backgroundPlayer.numberOfLoops = 0
+            backgroundPlayer.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+}
--- Metronome/ViewController.swift
+++ Metronome/ViewController.swift
@@ -10,16 +10,80 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progress: UIProgressView!
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var pulseLabel: UILabel!
+    @IBOutlet weak var bgBlue: UIView!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var playImage: UIImageView!
+    @IBOutlet weak var auxView: UIView!
+    @IBOutlet weak var compassLabel: UILabel!
+    
+    let max: Int = 240
+    let min: Int = 40
+    var timer: Timer = Timer()
+    var play: Bool = false
+    var compass: Int = 0
+    var compassCounter: Int = 0
+    
+    var pace: Int = 120 {
+        didSet {
+            pulseLabel.text = "\(pace) BPM"
+            progress.progress = Float(pace - min)/Float(max - min)
+            if play {
+                timer.invalidate()
+                self.startMetronome()
+            }
+        }
+    }
+    var pulse: Double {
+        return 60.0/Double(pace)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        bgBlue.layer.cornerRadius = bgBlue.frame.width/2
+        auxView.layer.cornerRadius = auxView.frame.width/2
+        pace = Int(stepper.value)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func stepperValueChanged(_ sender: Any) {
+        pace = Int(stepper.value)
     }
-
-
+    
+    func startMetronome() {
+        timer = Timer.scheduledTimer(timeInterval: pulse, target: self, selector: #selector(updateMetronome), userInfo: nil, repeats: true)
+    }
+    
+    func stopMetronome() {
+        timer.invalidate()
+    }
+    
+    @objc func updateMetronome() {
+        pulseView()
+        AudioController.playLowBeep()
+    }
+    
+    func pulseView() {
+        UIView.transition(with: bgBlue, duration: 0.1, options: .layoutSubviews, animations: {
+            self.bgBlue.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+        }, completion: { (_) in
+            UIView.transition(with: self.bgBlue, duration: 0.1, options: .layoutSubviews, animations: {
+                self.bgBlue.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            }, completion: nil)
+        })
+    }
+    
+    @IBAction func pressStart(_ sender: Any) {
+        play = !play
+        if play {
+            playImage.image = #imageLiteral(resourceName: "PauseOnly")
+            self.startMetronome()
+        } else {
+            playImage.image = #imageLiteral(resourceName: "PlayOnly")
+            self.stopMetronome()
+        }
+    }
 }
+
 
